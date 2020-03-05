4d6a0607ab2cd837dd405756b7876f47bb0a3bbe
BlaineAndersonDev <BlaineAndersonDev@Gmail.com>
2020-03-05 13:40:10 -0700

    Updated Landing to include components and navigation

---
+++ src/components/navigation.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { Link } from "gatsby"
+import Style from "./navigation.module.css"
+// Image imports.
+import PPLogo from '../images/pplogo.png';
+
+export default () => (
+  <div className={Style.wrapper}>
+    <div className={Style.leftSide}>
+      <Link to="/" className={Style.link}>
+        <img 
+        src={PPLogo}
+        className={Style.logo}
+        alt="Logo" />
+      </Link>
+    </div>
+    <div className={Style.rightSide}>
+      <Link to="/" className={Style.link}>Filler2</Link>
+      <Link to="/" className={Style.link}>Filler1</Link>
+      <Link to="/" className={Style.link}>Filler3</Link>
+    </div>
+  </div>
+);
---
+++ src/pages/landing.js
@@ -0,0 +1,19 @@
+// Required imports.
+import React from "react";
+// Component & Styling imports.
+import Style from "./landing.module.css";
+// Image imports.
+import UspsTruck from '../images/postal_truck.jpg';
+
+export default () => (
+  <div className={Style.wrapper}>
+    <img 
+      src={UspsTruck}
+      className={Style.imageUnderlay}
+      alt="LandingImage" />
+    <div className={Style.textOverlay}> 
+      <h1 className={Style.title}>Power Postal</h1>
+      <p className={Style.subtitle}>Affordable & Reliable Shipping Services</p>
+    </div>
+  </div>
+);
