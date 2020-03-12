3dfc01eb638a42b5ab32ccf6f0da652c682928e9
BlaineAndersonDev <BlaineAndersonDev@Gmail.com>
2020-03-12 11:13:58 -0700

    Updated Landing to increase loading time for image

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -2,15 +2,9 @@
 import React from "react";
 // Component & Styling imports.
 import Style from "./landing.module.css";
-// Image imports.
-import UspsTruck from '../images/postal_truck_large.jpg';
 
 export default () => (
   <div className={Style.wrapper}>
-    <img 
-      src={UspsTruck}
-      className={Style.imageUnderlay}
-      alt="LandingImage" />
     <div className={Style.textOverlay}> 
       <h1 className={Style.title}>Power Postal</h1>
       <p className={Style.subtitle}>Affordable & Reliable Shipping Services</p>
