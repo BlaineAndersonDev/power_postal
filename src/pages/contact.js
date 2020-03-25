211de7e86da546288e8aaed69772d2c7db2fe25c
BlaineAndersonDev <BlaineAndersonDev@Gmail.com>
2020-03-25 00:59:52 -0700

    Updated colors and styles

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -11,21 +11,6 @@ export default () => (
     <div className={Style.section}>
 
       <div className={Style.subsection}>
-        <img src={iconHours} alt="icon" className={Style.icon} />
-        <p className={Style.title}>Hours</p>
-        <p className={Style.text}><span className={Style.bold}>Mon-Fri:</span> 8:30AM - 6:00PM</p>
-        <p className={Style.text}><span className={Style.bold}>Sat:</span> 9:30AM - 4:00PM</p>
-        <p className={Style.text}><span className={Style.bold}>Sun:</span> Closed</p>
-        <p className={Style.subtitle}>Holiday Hours</p>
-        <p className={Style.text}><span className={Style.bold}>Thanksgiving:</span> Closed</p>
-        <p className={Style.text}><span className={Style.bold}>Black Friday:</span> Closed</p>
-        <p className={Style.text}><span className={Style.bold}>Christmas Eve:</span> Closed</p>
-        <p className={Style.text}><span className={Style.bold}>Christmas Day:</span> Closed</p>
-        <p className={Style.text}><span className={Style.bold}>New Years Day:</span> Closed</p>
-        <p className={Style.text}>Call for additional holiday hours</p>
-      </div>
-
-      <div className={Style.subsection}>
         <img src={iconCarriers} alt="icon" className={Style.icon} />
         <p className={Style.title}>Carrier Pickup Times</p>
         <p className={Style.text}><span className={Style.bold}>FedEx Ground:</span> 4:00PM</p>
@@ -33,6 +18,15 @@ export default () => (
         <p className={Style.text}><span className={Style.bold}>UPS Ground:</span> 4:00PM</p>
         <p className={Style.text}><span className={Style.bold}>UPS Express:</span> 4:00PM</p>
         <p className={Style.text}><span className={Style.bold}>USPS:</span> Varies Daily (usually around 3:00PM)</p>
+      </div>
+
+      <div className={Style.subsection}>
+        <img src={iconHours} alt="icon" className={Style.icon} />
+        <p className={Style.title}>Hours</p>
+        <p className={Style.text}><span className={Style.bold}>Mon-Fri:</span> 8:30AM - 6:00PM</p>
+        <p className={Style.text}><span className={Style.bold}>Sat:</span> 9:30AM - 4:00PM</p>
+        <p className={Style.text}><span className={Style.bold}>Sun:</span> Closed</p>
+        <p className={Style.text}>Closed on all major holidays</p>
       </div>
 
     </div>
--- src/pages/footer.js
+++ src/pages/footer.js
@@ -11,48 +11,51 @@ import PartnerLeaninTree from '../images/partner_leanin_tree.png';
 export default () => (
   <div className={Style.wrapper}>
 
-    <div id={Style.locationBox} className={Style.section}>
+    <div id={Style.addressBox} className={Style.section}>
+      <p className={Style.title}>Contact Us</p>
       <div className={Style.subsection}>
-        <p className={Style.title}>Location</p>
         <p className={Style.text}><span className={Style.bold}>Power Postal At Las Sendas</span></p>
         <p className={Style.text}>2824 N Power Rd</p>
         <p className={Style.text}>Suite 113 Mesa, AZ 85215</p>
+      </div>
+      <div className={Style.subsection}>
         <p className={Style.text}><span className={Style.bold}>Email: </span><a href="mailto:info@powerpostal.com" className={Style.subtleButton}>Info@PowerPostal.com</a></p>
         <p className={Style.text}><span className={Style.bold}>Phone: </span>480-654-2600</p>
         <p className={Style.text}><span className={Style.bold}>Fax: </span>480-654-2800</p>
-        <Button text="Get Directions" link="https://www.google.com/maps/place/Power+Postal+at+Las+Sendas,+2824+N+Power+Rd+%23113,+Mesa,+AZ+85215/@33.466985,-111.685576,16z/data=!4m2!3m1!1s0x872ba5238ac70ab7:0x575496cc1e835641?hl=en-US&gl=US"/>
+        <a href="https://www.google.com/maps/place/Power+Postal+at+Las+Sendas,+2824+N+Power+Rd+%23113,+Mesa,+AZ+85215/@33.466985,-111.685576,16z/data=!4m2!3m1!1s0x872ba5238ac70ab7:0x575496cc1e835641?hl=en-US&gl=US" className={Style.subtleButton}><span className={Style.bold}>Get Directions</span></a>
       </div>
     </div>
 
-    <div id={Style.partnerBox} className={Style.section}>
-      <p className={Style.title}>Proudly Partnered With:</p>
-      <a href="https://www.leanintree.com/home.aspx" target="_blank" rel="noopener noreferrer">
-        <img 
-          src={PartnerLeaninTree}
-          className={Style.partnerLogo}
-          alt="LeaninTree" 
-        />
-      </a>
-      <a href="https://eggheadcomputerservices.com/home" target="_blank" rel="noopener noreferrer">
-        <img 
-          src={PartnerEgghead}
-          className={Style.partnerLogo}
-          alt="Egghead" 
-        />
-      </a>
-      <a href="http://milanomusic.com/" target="_blank" rel="noopener noreferrer">
-        <img 
-          src={PartnerMilano}
-          className={Style.partnerLogo}
-          alt="Milano" 
-        />
-      </a>
+    <div className={Style.section}>
+      <p id={Style.partnerTitle} className={Style.title}>Proudly Partnered With:</p>
+      <div id={Style.partnerBox}>
+        <a href="https://www.leanintree.com/home.aspx" target="_blank" rel="noopener noreferrer">
+          <img 
+            src={PartnerLeaninTree}
+            className={Style.partnerLogo}
+            alt="LeaninTree" 
+          />
+        </a>
+        <a href="https://eggheadcomputerservices.com/home" target="_blank" rel="noopener noreferrer">
+          <img 
+            src={PartnerEgghead}
+            className={Style.partnerLogo}
+            alt="Egghead" 
+          />
+        </a>
+        <a href="http://milanomusic.com/" target="_blank" rel="noopener noreferrer">
+          <img 
+            src={PartnerMilano}
+            className={Style.partnerLogo}
+            alt="Milano" 
+          />
+        </a>
+      </div>
     </div>
 
     <div id={Style.bottomBox} className={Style.section}>
-      <p id={Style.bottomText} className={Style.text}>© 2020 Power Postal at Las Sendas</p>
-      <p id={Style.bottomText} className={Style.text}>Questions? <Button text="Contact Us" link="mailto:info@powerpostal.com"/></p>
-      <p className={Style.textSmall}>Built by <a href="http://blaineandersondev.com/" className={Style.subtleButton}>Blaine Anderson Development LLC</a></p>
+      <p className={Style.textSmall}>© 2020 Power Postal at Las Sendas</p>
+      <p className={Style.textSmall}><a href="http://blaineandersondev.com/" className={Style.subtleButton}>Blaine Anderson Development LLC</a></p>
     </div>
     
   </div>
